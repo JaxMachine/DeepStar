4e9a3b53699d4978e915488d71e64b9b82a92702
peter <pwensel@gmail.com>
2016-04-23 10:19:25 -0500

    original camera

--- objects/camera_center.py
+++ objects/camera_center.py
@@ -1,4 +1,3 @@
-import pygame
 
 from objects.game_object import BaseObject
 
@@ -13,37 +12,19 @@ class CameraCenter(BaseObject):
         BaseObject.__init__(self, sprite_name, (target.pos.x, target.pos.y), SPRITE_MANAGER.instance)
         self.target = target
         self.pos = self.target.pos
-        self.current_dist = self.target.radius
         self._update_pos()
-
         # self.dirty = 0
 
     # how should I do this?...
 
     def _update_pos(self):
-        min_dist = self.target.radius - 5
-        max_dist = 350
-        if self.target.IsMoving():
-            if self.current_dist < 200:
-                self.current_dist += 5
-            else:
-                self.current_dist += 1
-            if self.current_dist > max_dist:
-                self.current_dist = max_dist
-        else:
-            self.current_dist -= 10
-            if self.current_dist < min_dist:
-                self.current_dist = min_dist
-
         direction = self.target.getPlayerDirection()
-        # spawn_distance = -300
+        spawn_distance = -150
         # radi = self.target.radius
-        self.pos = self.target.pos + (direction * -self.current_dist)
-        # p = self.target.pos + (direction * -self.current_dist)
-        # p = CAMERA.apply_point((int(p.x), int(p.y)))
-        p = self.target.pos + (direction * -min_dist)
-        p = CAMERA.apply_point((int(p.x), int(p.y)))
-        pygame.draw.circle(SCREEN, (0, 255, 255), p, 5)
+        self.pos = self.target.pos + (direction * spawn_distance)
+        # p = self.target.pos + (direction * radi)
+        # p = CAMERA.apply_point(p)
+        # pygame.draw.circle(SCREEN, (0, p )
 
     def update(self):
         self._update_pos()
